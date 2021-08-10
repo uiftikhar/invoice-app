4a8ecae109b75750b41235cfc3dd3a6f82a0c2fa
umer <umer229@gmail.com>
2021-08-10 12:21:41 +0200

    WIP

--- js/helpers.js
+++ js/helpers.js
@@ -21,6 +21,7 @@ const updateEditInvoice = (editInvoiceWrapper, data) => {
   const daysElement = document.querySelector('.date-picker .dates .days');
   
   datePickerElement.addEventListener('click', (e) => {
+    populateDates(daysElement,selectedDateElement , data);
     if (!checkEventPathForClass(e.path, 'dates')) {
       datesElement.classList.toggle('active');
     }
@@ -34,6 +35,54 @@ const updateEditInvoice = (editInvoiceWrapper, data) => {
     const form = Array.from(data.entries());
     updateItemsInLocalStorage(form);
   });
+}
+
+const  populateDates = (daysElement, selectedDateElement, data) => {
+  let date = new Date(data.createdAt);
+  let day = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+
+  let selectedDate = date;
+  let selectedDay = day;
+  let selectedMonth = month;
+  let selectedYear = year;
+	daysElement.innerHTML = '';
+	let amountDays = 31;
+
+  if(month % 2 !== 0) {
+    amountDays = 30;
+    if (month == 1) {
+      amountDays = 28;
+    }
+    if (month == 7) {
+      amountDays = 31;
+    }
+  }
+
+	for (let i = 0; i < amountDays; i++) {
+		const dayElement = document.createElement('div');
+		dayElement.classList.add('day');
+		dayElement.textContent = i + 1;
+
+		if (selectedDay == (i + 1) && selectedYear == year && selectedMonth == month) {
+			dayElement.classList.add('selected');
+		}
+
+		dayElement.addEventListener('click', function () {
+			selectedDate = new Date(year + '-' + (month + 1) + '-' + (i + 1));
+			selectedDay = (i + 1);
+			selectedMonth = month;
+			selectedYear = year;
+
+			selectedDateElement.textContent = formatDate(selectedDate);
+			selectedDateElement.dataset.value = selectedDate;
+
+			populateDates(daysElement, selectedDateElement, data);
+		});
+
+		daysElement.appendChild(dayElement);
+	}
 }
 
 const updateItemsInLocalStorage = (entries) => {
