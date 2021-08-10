f5f0804494f65755b94a8dc9d871422f7839e683
umer <umer229@gmail.com>
2021-08-10 18:53:05 +0200

    WC

--- assets/data.js
+++ assets/data.js
@@ -1,4 +1,4 @@
-[
+const data = [
   {
     "id": "RT3080",
     "createdAt": "2021-08-18",
--- js/loadData.js
+++ js/loadData.js
@@ -1,5 +1,5 @@
 (async function () {
-  const res = await fetch('/assets/data.json');
+  const res = await fetch('/assets/data.js');
   const jsonRes = await res.json();
   localStorage.setItem('data', JSON.stringify(jsonRes));
 }())
